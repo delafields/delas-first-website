438fe226ebb5a70d07031abe58ffcfb67632bcf5
badatfun <j.c.delafields@gmail.com>
2017-09-17 12:08:02 -0400

    Fresh start

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Vuetify from 'vuetify';
 import App from './App.vue';
 
 import { routes } from './routes';
+
+Vue.use(Vuetify);
 
 Vue.use(VueRouter);
 const router = new VueRouter({
@@ -12,5 +15,6 @@ const router = new VueRouter({
 
 new Vue({
 	el: '#app',
+	router,
 	render: h => h(App)
 });
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-import Home from "./components/Home.vue";
-import Projects from "./components/Projects.vue";
+import Home from './components/Home.vue';
+import Projects from './components/Projects.vue';
 
 export const routes = [
-  { path: "/", component: Home },
-  { path: "/projects", component: Projects }
+	{ path: '/', component: Home },
+	{ path: '/projects', component: Projects }
 ];
